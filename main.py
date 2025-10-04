dc041fb0fa988c5f90437164f357d3cecece3f8c
Reg Dunlop <misc@murtha.me>
2025-10-04 10:29:25 -0400

    foo bar

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from kivy.uix.label import Label
 
 class MyApp(App):
     def build(self):
-        return Label(text = "Symfix")
+        return Label(text = "MimicMotion")
     
 
 if __name__ == "__main__":
     MyApp().run()
+    print("Hello World")
